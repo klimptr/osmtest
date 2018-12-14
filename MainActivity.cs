16c3c9ddf3eaf01c78ec47713fdc4f384d4f983c
klimptr <45857444+klimptr@users.noreply.github.com>
2018-12-14 01:33:58 +0100

    Add files via upload

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Globalization;
 using Android.App;
 using Android.Graphics;
 using Android.OS;
 using Android.Support.V7.App;
+using Android.Views;
 using Android.Widget;
 using OsmSharp.Android.UI;
 using OsmSharp.Android.UI.Data.SQLite;
@@ -21,11 +23,12 @@ namespace App1
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
 
             Native.Initialize();
-
+            
             _mapView = new MapView(this, new MapViewSurface(this));
-            _mapView.MapTilt = 0; // must be set otherwise NullReferenceException
+            _mapView.MapTilt = 0;
             _mapView.MapCenter = new GeoCoordinate(53.770226, 20.490189);
             _mapView.MapZoom = 12;
             _mapView.Map = new Map();
@@ -37,18 +40,25 @@ namespace App1
                 var marker = new MapMarker(this, new GeoCoordinate(53.770226, 20.490189), MapMarkerAlignmentType.CenterBottom, bitmap);
                 _mapView.AddMarker(marker);
             }
-
-
-            var layout = new RelativeLayout(this);
+            var layout = FindViewById<RelativeLayout>(Resource.Id.Mapka);
             layout.AddView(_mapView);
+        }
 
+        [Java.Interop.Export("Goto")]
+        public void Goto(View v)
+        {
+            var latitude = FindViewById<EditText>(Resource.Id.latitude);
+            var longitude = FindViewById<EditText>(Resource.Id.longitude);
 
-            SetContentView(layout);
+            double latitudeValue = double.Parse(latitude.Text, CultureInfo.InvariantCulture);
+            double longitudeValue = double.Parse(longitude.Text, CultureInfo.InvariantCulture);
 
-
-
-
-
+            _mapView.MapCenter = new GeoCoordinate(latitudeValue, longitudeValue);
+            using (var bitmap = BitmapFactory.DecodeResource(Resources, Resource.Drawable.pin))
+            {
+                var marker = new MapMarker(this, new GeoCoordinate(latitudeValue, longitudeValue), MapMarkerAlignmentType.CenterBottom, bitmap);
+                _mapView.AddMarker(marker);
+            }
         }
     }
 }
